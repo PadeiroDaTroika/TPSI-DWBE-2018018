8796a384f37d7cd3b9b2dadb27b6dfc99ea9cc94
Leandro Moniz <2018018@student.uma.pt>
2025-02-04 18:50:05 +0000

    Create server.js

---
+++ Ficha_1/server.js
@@ -0,0 +1,40 @@
+//console.log("Hello World!");
+
+//function calculateGrade(p1,p2,freq){
+//    var endGrade = (p1 * 0.3 + p2 * 0.4 + freq * 0.3);
+//    console.log("A nota final é de: " + endGrade);
+//    if (endGrade >= 9.5)
+//        console.log("Aluno Aprovado");
+//    else
+//        console.log("Aluno Reprovado"); 
+//}
+
+//calculateGrade(20,10,9)
+
+const num = (1,2,3,4,5,6,7,8,9,10,11,12)
+switch(num){
+    case 1:
+        console.log("Janeiro");
+    case 2:
+        console.log("Fevereiro");
+    case 3:
+        console.log("Março");
+    case 4:
+        console.log("Abril");
+    case 5:
+        console.log("Maio");
+    case 6:
+        console.log("Junho");
+    case 7:
+        console.log("Julho");
+    case 8:
+        console.log("Agosto");
+    case 9:
+        console.log("Setembro");
+    case 10:
+        console.log("Outubro");
+    case 11:
+        console.log("Novembro");
+    case 12:
+        console.log("Dezembro");
+}
